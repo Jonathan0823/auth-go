d92d3b0681b18a54439a39b100f448db74ac513d
Guinn <eguinjonathan08@gmail.com>
2025-05-18 19:28:26 +0700

    feat(user): add user handler

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/Jonathan0823/auth-go/internal/dto"
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,4 +24,57 @@ func (h *MainHandler) GetUserByID(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "User retrieved successfully", "user": user})
+}
+
+func (h *MainHandler) GetAllUsers(c *gin.Context) {
+	users, err := h.svc.GetAllUsers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get users"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Users retrieved successfully", "users": users})
+}
+
+func (h *MainHandler) GetUserByEmail(c *gin.Context) {
+	email := c.Query("email")
+
+	user, err := h.svc.GetUserByEmail(email)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User retrieved successfully", "user": user})
+}
+
+func (h *MainHandler) UpdateUser(c *gin.Context) {
+	var user dto.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	if err := h.svc.UpdateUser(user, c); err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User updated successfully"})
+}
+
+func (h *MainHandler) DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	if err := h.svc.DeleteUser(idInt, c); err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted successfully"})
 }
