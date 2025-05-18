ad672e6be61db9f2225087d5c1291ad777abb455
Guinn <eguinjonathan08@gmail.com>
2025-05-18 19:28:16 +0700

    feat(user): add some user svc

--- internal/service/main_svc.go
+++ internal/service/main_svc.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/Jonathan0823/auth-go/internal/dto"
 	"github.com/Jonathan0823/auth-go/internal/repository"
+	"github.com/gin-gonic/gin"
 )
 
 type Service interface {
@@ -12,6 +13,10 @@ type Service interface {
 
 	// user service
 	GetUserByID(id int) (dto.User, error)
+	GetUserByEmail(email string) (dto.User, error)
+	GetAllUsers() ([]dto.User, error)
+	UpdateUser(user dto.User, c *gin.Context) error
+	DeleteUser(id int, c *gin.Context) error
 }
 
 type service struct {
--- internal/service/user_svc.go
+++ internal/service/user_svc.go
@@ -1,7 +1,47 @@
 package service
 
-import "github.com/Jonathan0823/auth-go/internal/dto"
+import (
+	"fmt"
+
+	"github.com/Jonathan0823/auth-go/internal/dto"
+	"github.com/Jonathan0823/auth-go/utils"
+	"github.com/gin-gonic/gin"
+)
 
 func (s *service) GetUserByID(id int) (dto.User, error) {
 	return s.repo.GetUserByID(id)
+}
+
+func (s *service) GetUserByEmail(email string) (dto.User, error) {
+	return s.repo.GetUserByEmail(email)
+}
+
+func (s *service) GetAllUsers() ([]dto.User, error) {
+	return s.repo.GetAllUsers()
+}
+
+func (s *service) UpdateUser(user dto.User, c *gin.Context) error {
+	currentUser, err := utils.GetUser(c)
+	if err != nil {
+		return fmt.Errorf("User is not found")
+	}
+
+	if currentUser.ID != user.ID {
+		return fmt.Errorf("You are not authorized to update this user")
+	}
+
+	return s.repo.UpdateUser(user)
+}
+
+func (s *service) DeleteUser(id int, c *gin.Context) error {
+	currentUser, err := utils.GetUser(c)
+	if err != nil {
+		return fmt.Errorf("User is not found")
+	}
+
+	if currentUser.ID != id {
+		return fmt.Errorf("You are not authorized to update this user")
+	}
+
+	return s.repo.DeleteUser(id)
 }
