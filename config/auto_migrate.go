46df72923fbffb6226bd9c818e5b4661c2ba6c2c
Guinn <eguinjonathan08@gmail.com>
2025-05-19 07:10:57 +0700

    fix(auto migrate): ensure correct column name in users table

--- config/auto_migrate.go
+++ config/auto_migrate.go
@@ -6,7 +6,7 @@ func AutoMigrate(db *sql.DB) error {
 	if _, err := db.Exec(`
     CREATE TABLE IF NOT EXISTS users ( 
       id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
+      username VARCHAR(100) NOT NULL,
       email VARCHAR(100) NOT NULL UNIQUE,
       password VARCHAR(100) NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
