d7360751e0d1e8e2c1d9fc92ee5a34b004eacf02
Guinn <eguinjonathan08@gmail.com>
2025-05-21 04:38:49 +0700

    feat(auto migrate): add table verify emails

--- config/auto_migrate.go
+++ config/auto_migrate.go
@@ -15,5 +15,14 @@ func AutoMigrate(db *sql.DB) error {
 		return err
 	}
 
+	if _, err := db.Exec(`
+    CREATE TABLE IF NOT EXISTS verify_emails ( 
+      id UUID PRIMARY KEY, 
+      email VARCHAR(100) NOT NULL,
+      expired_at TIMESTAMP NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    )`); err != nil {
+		return err
+	}
 	return nil
 }
