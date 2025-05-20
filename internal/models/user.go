b90c195da36aae6a461912a7811c0c6a1194c3de
Guinn <eguinjonathan08@gmail.com>
2025-05-21 04:58:16 +0700

    feat(user): include isverified and image url to the type

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,10 +1,12 @@
 package models
 
 type User struct {
-	ID        int    `json:"id"`
-	Username  string `json:"username"`
-	Email     string `json:"email"`
-	Password  string `json:"password,omitempty"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID         int    `json:"id"`
+	Username   string `json:"username"`
+	ImageURL   string `json:"image_url"`
+	Email      string `json:"email"`
+	Password   string `json:"password,omitempty"`
+	IsVerified bool   `json:"is_verified"`
+	CreatedAt  string `json:"created_at"`
+	UpdatedAt  string `json:"updated_at"`
 }
