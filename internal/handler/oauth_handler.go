9d5a64ac0cf35eb9100175ee88d70c9aba71fc91
Guinn <eguinjonathan08@gmail.com>
2025-05-26 20:34:58 +0700

    feat(oauth_handler): implement oauth handler for login

--- internal/handler/oauth_handler.go
+++ internal/handler/oauth_handler.go
@@ -1,27 +1,33 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/Jonathan0823/auth-go/internal/models"
 	"github.com/gin-gonic/gin"
 	"github.com/markbates/goth/gothic"
 )
 
-func (h *MainHandler) OAuthCallback(c *gin.Context) {
+func (h *MainHandler) OAuthLogin(c *gin.Context) {
 	gothic.BeginAuthHandler(c.Writer, c.Request)
 }
 
-func (h *MainHandler) OAuthLogin(c *gin.Context) {
+func (h *MainHandler) OAuthCallback(c *gin.Context) {
 	user, err := gothic.CompleteUserAuth(c.Writer, c.Request)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to login"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	fmt.Println("User logged in:", user)
+	data := models.User{
+		OAuthID:  user.UserID,
+		Email:    user.Email,
+		Username: user.NickName,
+		Provider: user.Provider,
+	}
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "User logged in successfully",
+		"user":    data,
 	})
 }
