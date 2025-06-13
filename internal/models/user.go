a3a33de9eb15cfc6809bdb401e111db2d4969996
Guinn <eguinjonathan08@gmail.com>
2025-06-13 10:56:08 +0700

    feat(model): update user model and updateuserequest to make their username optional

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,7 +3,7 @@ package models
 type User struct {
 	ID         int    `json:"id"`
 	OAuthID    string `json:"oauth_id,omitempty"`
-	Username   string `json:"username" validate:"required,min=3,max=30,alphanum"`
+	Username   string `json:"username" validate:"omitempty,min=3,max=30"`
 	AvatarURL  string `json:"avatar_url,omitempty"`
 	Email      string `json:"email" validate:"required,email"`
 	Password   string `json:"password,omitempty" validate:"required_without=OAuthID,min=8,max=100"`
@@ -15,7 +15,7 @@ type User struct {
 
 type UpdateUserRequest struct {
 	ID        int    `json:"id" validate:"required"`
-	Username  string `json:"username" validate:"required,min=3,max=30,alphanum"`
+	Username  string `json:"username" validate:"omitempty,min=3,max=30"`
 	AvatarURL string `json:"avatar_url,omitempty"`
 	Email     string `json:"email" validate:"required,email"`
 }
