fa24a4c3a37d4ade184d80f6f1d913392ced896b
Guinn <eguinjonathan08@gmail.com>
2025-06-13 10:21:46 +0700

    feat(models): add validation to user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,10 +3,10 @@ package models
 type User struct {
 	ID         int    `json:"id"`
 	OAuthID    string `json:"oauth_id,omitempty"`
-	Username   string `json:"username"`
+	Username   string `json:"username" validate:"required,min=3,max=30,alphanum"`
 	AvatarURL  string `json:"avatar_url,omitempty"`
-	Email      string `json:"email"`
-	Password   string `json:"password,omitempty"`
+	Email      string `json:"email" validate:"required,email"`
+	Password   string `json:"password,omitempty" validate:"required_without=OAuthID,min=8,max=100"`
 	IsVerified bool   `json:"is_verified"`
 	Provider   string `json:"provider,omitempty"`
 	CreatedAt  string `json:"created_at"`
