8809848d7788e948ca1e9f355753328d46e97094
Jonathan0823 <eguinjonathan08@gmail.com>
2025-08-14 09:59:19 +0700

    feat(errors): update error struct to accept original error

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -8,50 +8,65 @@ import (
 type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
+	Err     error  `json:"-"`
 }
 
 func (e *Error) Error() string {
 	return e.Message
 }
 
-func New(code int, message string) *Error {
+func New(code int, message string, err error) *Error {
 	return &Error{
 		Code:    code,
 		Message: message,
+		Err:     err,
 	}
 }
 
-func Unauthorized(message string) *Error {
+func Unauthorized(message string, err error) *Error {
 	return &Error{
 		Code:    http.StatusUnauthorized,
 		Message: message,
+		Err:     err,
 	}
 }
 
-func NotFound(message string) *Error {
+func NotFound(message string, err error) *Error {
 	return &Error{
 		Code:    http.StatusNotFound,
 		Message: message,
+		Err:     err,
 	}
 }
 
-func BadRequest(message string) *Error {
+func BadRequest(message string, err error) *Error {
 	return &Error{
 		Code:    http.StatusBadRequest,
 		Message: message,
+		Err:     err,
 	}
 }
 
-func InternalServerError(message string) *Error {
+func InternalServerError(message string, err error) *Error {
 	return &Error{
 		Code:    http.StatusInternalServerError,
 		Message: message,
+		Err:     err,
 	}
 }
 
-func Conflict(message string) *Error {
+func Conflict(message string, err error) *Error {
 	return &Error{
 		Code:    http.StatusConflict,
 		Message: message,
+		Err:     err,
+	}
+}
+
+func Forbidden(message string, err error) *Error {
+	return &Error{
+		Code:    http.StatusForbidden,
+		Message: message,
+		Err:     err,
 	}
 }
