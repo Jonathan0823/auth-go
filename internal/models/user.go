3043e84786d942d908d6dbcc66d9ac96b21a10fe
Jonathan0823 <eguinjonathan08@gmail.com>
2025-08-15 10:08:02 +0700

    refactor(user): update some field type

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,18 +1,20 @@
 package models
 
+import "time"
+
 type User struct {
-	ID         int    `json:"id"`
-	OAuthID    string `json:"oauth_id,omitempty"`
-	Username   string `json:"username" validate:"omitempty,min=3,max=30"`
-	AvatarURL  string `json:"avatar_url,omitempty"`
-	Email      string `json:"email" validate:"required,email"`
-	Password   string `json:"password,omitempty" validate:"required_without=OAuthID,min=8,max=100"`
-	IsVerified bool   `json:"is_verified"`
-	Provider   string `json:"provider,omitempty"`
-	IPAddress  string `json:"ip_address,omitempty"`
-	UserAgent  string `json:"user_agent,omitempty"`
-	CreatedAt  string `json:"created_at"`
-	UpdatedAt  string `json:"updated_at"`
+	ID         int       `json:"id"`
+	OAuthID    string    `json:"oauth_id,omitempty"`
+	Username   string    `json:"username" validate:"omitempty,min=3,max=30"`
+	AvatarURL  string    `json:"avatar_url,omitempty"`
+	Email      string    `json:"email" validate:"required,email"`
+	Password   string    `json:"password,omitempty" validate:"required_without=OAuthID,min=8,max=100"`
+	IsVerified bool      `json:"is_verified"`
+	Provider   string    `json:"provider,omitempty"`
+	IPAddress  string    `json:"ip_address,omitempty"`
+	UserAgent  string    `json:"user_agent,omitempty"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
 
 type UpdateUserRequest struct {
