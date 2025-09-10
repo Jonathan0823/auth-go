75669869f9d829786c0f664898f4419cf52f28f7
Jonathan0823 <eguinjonathan08@gmail.com>
2025-09-10 12:35:16 +0700

    feat(repo): implement dynamic repository between using db or tx

--- internal/repository/main_repo.go
+++ internal/repository/main_repo.go
@@ -1,41 +1,44 @@
-// Package repository provides methods for interacting with the database related to email verification and password reset functionalities
+// Package repository is the main repository package that provides access to all repositories and transaction management.
 package repository
 
 import (
+	"context"
 	"database/sql"
-
-	"github.com/Jonathan0823/auth-go/internal/models"
 )
 
-type Repository interface {
-	// auth repo
-	CreateVerifyEmail(verifyEmail models.VerifyEmail) error
-	GetVerifyEmailByID(id string) (models.VerifyEmail, error)
-	VerifyEmail(id string) error
-	CreateForgotPasswordEmail(data models.ForgotPassword) error
-	GetForgotPasswordByID(id string) (models.ForgotPassword, error)
-	DeleteForgotPasswordByID(id string) error
-	CreateTokenLog(tokenLog models.TokenLog) error
-	GetTokenLogByJTI(jti string) (models.TokenLog, error)
-	InvalidateTokenLog(oldJti, newJti string) error
-	IsTokenLogInvalidated(jti string) (bool, error)
+type Repository struct{ db *sql.DB }
 
-	// user repo
-	GetUserByID(id int) (*models.User, error)
-	GetUserByEmail(email string, includePassword bool) (*models.User, error)
-	CreateUser(user models.User) error
-	GetAllUsers() ([]*models.User, error)
-	UpdateUser(user models.UpdateUserRequest) error
-	DeleteUser(id int) error
-	UpdateUserPassword(id int, newPassword string) error
+func NewRepository(db *sql.DB) *Repository { return &Repository{db: db} }
+
+func (r *Repository) Auth() AuthRepository  { return NewAuthRepository(r.db) }
+func (r *Repository) Users() UserRepository { return NewUserRepository(r.db) }
+
+type UOW struct {
+	tx *sql.Tx
 }
 
-type repository struct {
-	db *sql.DB
-}
-
-func NewRepository(db *sql.DB) Repository {
-	return &repository{
-		db: db,
+func (r *Repository) Begin(ctx context.Context, opts *sql.TxOptions) (*UOW, error) {
+	tx, err := r.db.BeginTx(ctx, opts)
+	if err != nil {
+		return nil, err
 	}
+	return &UOW{tx: tx}, nil
+}
+func (u *UOW) Commit() error   { return u.tx.Commit() }
+func (u *UOW) Rollback() error { return u.tx.Rollback() }
+
+func (u *UOW) Users() UserRepository { return NewUserRepository(u.tx) }
+func (u *UOW) Auth() AuthRepository  { return NewAuthRepository(u.tx) }
+
+func (r *Repository) WithTx(ctx context.Context, fn func(u *UOW) error) error {
+	u, err := r.Begin(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer u.Rollback()
+
+	if err := fn(u); err != nil {
+		return err
+	}
+	return u.Commit()
 }
