dbc6adf3ee8962a9949ac00239f6122a8a42dcb8
Jonathan0823 <eguinjonathan08@gmail.com>
2025-09-10 14:34:10 +0700

    feat(main svc): update the service struct to became service group

--- internal/service/main_svc.go
+++ internal/service/main_svc.go
@@ -1,40 +1,16 @@
 package service
 
 import (
-	"github.com/Jonathan0823/auth-go/internal/models"
 	"github.com/Jonathan0823/auth-go/internal/repository"
-	"github.com/gin-gonic/gin"
 )
 
-type Service interface {
-	// auth service
-	Register(user models.User) error
-	Login(user models.User) (string, string, error)
-	ForgotPassword(email string) error
-	CreateVerifyEmail(email string) error
-	VerifyEmail(id string, c *gin.Context) error
-	ResetPassword(tokenStr string, newPassword string) error
-	RefreshTokens(refreshToken, ip, userAgent string) (string, string, error)
-	InvalidateJWTTokens(oldJTI, newJTI string) error
-	IsTokenLogInvalidated(jti string) (bool, error)
-
-	// oauth service
-	OAuthLogin(user models.User) (*models.User, error)
-
-	// user service
-	GetUserByID(id int) (*models.User, error)
-	GetUserByEmail(email string) (*models.User, error)
-	GetAllUsers() ([]*models.User, error)
-	UpdateUser(user models.UpdateUserRequest, c *gin.Context) error
-	DeleteUser(id int, c *gin.Context) error
-}
-
 type service struct {
-	repo repository.Repository
+	oauth OAuthService
 }
 
-func NewService(repo repository.Repository) Service {
+func NewService(repo repository.Repository) *service {
+	oauthSvc := NewOAuthService(repo)
 	return &service{
-		repo: repo,
+		oauth: oauthSvc,
 	}
 }
