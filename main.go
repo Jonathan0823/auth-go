122359fe9d289ed32c585f067e15042179354be9
Jonathan0823 <eguinjonathan08@gmail.com>
2025-09-13 16:23:52 +0700

    refactor(main) load env only in dev

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Jonathan0823/auth-go/config"
 	"github.com/Jonathan0823/auth-go/internal/handler"
@@ -14,9 +15,11 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("ENVIRONMENT") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 
 	config.InitOAuth()
