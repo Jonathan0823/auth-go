8cbae8e3512b00a7c924ee21eef8c98ff36a1125
Jonathan0823 <eguinjonathan08@gmail.com>
2026-07-30 15:03:42 +0700

    fix(domain): decouple errors from http status codes

--- internal/core/domain/errors.go
+++ internal/core/domain/errors.go
@@ -1,7 +1,12 @@
 package domain
 
-import (
-	"net/http"
+const (
+	ErrCodeInternal     = 1
+	ErrCodeBadRequest   = 2
+	ErrCodeNotFound     = 3
+	ErrCodeConflict     = 4
+	ErrCodeUnauthorized = 5
+	ErrCodeForbidden    = 6
 )
 
 type Error struct {
@@ -10,71 +15,41 @@ type Error struct {
 	Err     error  `json:"-"`
 }
 
-func (e *Error) Error() string {
-	return e.Message
-}
+func (e *Error) Error() string { return e.Message }
 
 func NewError(code int, message string, err error) *Error {
-	return &Error{
-		Code:    code,
-		Message: message,
-		Err:     err,
-	}
+	return &Error{Code: code, Message: message, Err: err}
 }
 
 func Unauthorized(message string, err error) *Error {
-	return &Error{
-		Code:    http.StatusUnauthorized,
-		Message: message,
-		Err:     err,
-	}
+	return &Error{Code: ErrCodeUnauthorized, Message: message, Err: err}
 }
 
 func NotFound(message string, err error) *Error {
-	return &Error{
-		Code:    http.StatusNotFound,
-		Message: message,
-		Err:     err,
-	}
+	return &Error{Code: ErrCodeNotFound, Message: message, Err: err}
 }
 
 func BadRequest(message string, err error) *Error {
-	return &Error{
-		Code:    http.StatusBadRequest,
-		Message: message,
-		Err:     err,
-	}
+	return &Error{Code: ErrCodeBadRequest, Message: message, Err: err}
 }
 
 func InternalServerError(message string, err error) *Error {
-	return &Error{
-		Code:    http.StatusInternalServerError,
-		Message: message,
-		Err:     err,
-	}
+	return &Error{Code: ErrCodeInternal, Message: message, Err: err}
 }
 
 func Conflict(message string, err error) *Error {
-	return &Error{
-		Code:    http.StatusConflict,
-		Message: message,
-		Err:     err,
-	}
+	return &Error{Code: ErrCodeConflict, Message: message, Err: err}
 }
 
 func Forbidden(message string, err error) *Error {
-	return &Error{
-		Code:    http.StatusForbidden,
-		Message: message,
-		Err:     err,
-	}
+	return &Error{Code: ErrCodeForbidden, Message: message, Err: err}
 }
 
 func ErrorCode(err error) int {
 	if e, ok := err.(*Error); ok {
 		return e.Code
 	}
-	return http.StatusInternalServerError
+	return ErrCodeInternal
 }
 
 func ErrorMessage(err error) string {
